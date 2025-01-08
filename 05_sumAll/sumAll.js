c4ecb3b13105668a62799c0d8db15d7272bcc944
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-08 15:12:33 -0500

    Done Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a)||!Number.isInteger(b)){
+        return 'ERROR';
+    }
+    if(a > b){
+        let temp = b;
+        b = a;
+        a = temp;
+    }
+    if(a < 0){
+        return 'ERROR';
+    }
+    return (a + b)*(b-a+1)/2;
 };
 
 // Do not edit below this line
