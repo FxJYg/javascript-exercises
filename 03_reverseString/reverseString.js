e181059311988add64cdd80aed2944de97cc4a65
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-08 14:49:57 -0500

    Done Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rev = '';
+    for(let cur = str.length-1; cur >=0 ; cur--){
+        rev = rev + str[cur];
+    }
+    return rev;
 };
 
 // Do not edit below this line
