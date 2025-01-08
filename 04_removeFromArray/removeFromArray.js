596c3512ce04dd636d675b9df07a8c5b11545992
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-08 14:59:27 -0500

    Done Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+
+let myArr = [];
+for(let i  = 0; i < arr.length; i++){
+    let work = 1;
+    for(let j = 1; j < arguments.length;j++){
+        if(arguments[j]===arr[i]){
+            work = 0;
+        }
+    }
+    if(work){
+        myArr.push(arr[i]);
+    }
+}
+return myArr;
 };
 
 // Do not edit below this line
