ca111b1bd80efe85ef73cf5062ec2209c0c35d61
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-10 14:03:20 -0500

    Done Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,5 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return n < 0 ? 'OOPS' : n==0 ? 0: n==1? 1 : n==2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
