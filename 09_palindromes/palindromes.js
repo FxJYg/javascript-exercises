7382a16e9bbe8d46c9999c4dd499316037f494e0
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-10 13:58:41 -0500

    Done Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+    const newStr = str.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('').toLowerCase();
+    const len = newStr.length-1;
+    for(let i = 0; i < len/2; i++){
+        if(newStr[i]!==newStr[len-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
