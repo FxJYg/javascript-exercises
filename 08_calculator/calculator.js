1c9e1e5776fa4db4c46ca66f342ef90609851608
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-10 13:41:37 -0500

    Done Exercies 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((cur,element)=> element+cur,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((cur,element)=>cur*element,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let ans = 1;
+  for(let i = 2; i <= n; i++){
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
