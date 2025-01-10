f592b9d67a1b2131a8a1a67789871db3e67343ca
Felix Yang <116753515+FxJYg@users.noreply.github.com>
2025-01-10 14:15:45 -0500

    Done Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((old, cur)=> {
+            if(cur.yearOfDeath==undefined){
+                cur.yearOfDeath = 2025;
+            }
+            if(old.yearOfDeath == undefined){
+                old.yearOfDeath = 2025;
+            }
+            return old.yearOfDeath - old.yearOfBirth > cur.yearOfDeath - cur.yearOfBirth? old: cur;
+    });
 };
 
 // Do not edit below this line
